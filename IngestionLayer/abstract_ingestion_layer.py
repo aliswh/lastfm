509368e2c500a3c9fbaa48675a8a4ed394b8b49d
Davide D'Ascenzo <davide.dascenzo.work@gmail.com>
2022-02-15 17:58:44 +0100

    Update abstract_ingestion_layer.py

--- IngestionLayer/abstract_ingestion_layer.py
+++ IngestionLayer/abstract_ingestion_layer.py
@@ -2,21 +2,17 @@ from abc import ABC, abstractmethod
  
 class Source(ABC):
     @abstractmethod
-    def __init__(self, fileType:str):
-        pass
-
-    @abstractmethod
-    def Get(request:str) -> str:
+    def Get(self, request) -> str:
         """
         Process a request and return a path containing the result.
 
         """
-        pass
+        pass 
 
 
 class DataLake(ABC):
     @abstractmethod
-    def Write(file_path:str, dest_path:str):
+    def Write(self, file_path:str, dest_path:str):
         """
         Write a file in the specified destination
 
@@ -28,13 +24,22 @@ class Writer(ABC):
     @abstractmethod
     def __init__(self, source:Source, destination:DataLake):
         pass
-    
+
     @abstractmethod
-    def Write(request:str, dest_path:str):
+    def Write(self, request, dest_path:str):
         """
         Process a request and save in destination.
 
         """
         pass
         
+        
+class BatchWriter(Writer):
+    def __init__(self, source, destination):
+        self.source = source
+        self.dest = destination
+
+    def Write(self, request, dest_path):
+        file_path = self.source.Get(request)
+        self.dest.Write(file_path, dest_path)
 
