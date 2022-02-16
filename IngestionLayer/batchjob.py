3a3de4906feb7b86ead268ae5e10fcf78e4653a1
Davide D'Ascenzo <davide.dascenzo.work@gmail.com>
2022-02-16 15:47:12 +0100

    Updated batchjob and created requirements

--- IngestionLayer/batchjob.py
+++ IngestionLayer/batchjob.py
@@ -5,11 +5,12 @@ from config import *
 storage = GoogleStorage(creds_path, bucket_name)
 source = PyLastSource(AUTH_DATA)
 
-users = source.get('users',False,'alishw')
+SEED_USER = 'dars4'
+users = source.get('users',SEED_USER)
 for json_user in users:
     storage.write(json_user, f'user_{json_user.id}')
-    json_recent_tracks = source.get('recent_tracks', False,json_user.id)
+    json_recent_tracks = source.get('recent_tracks',json_user.name)
     storage.write(json_recent_tracks, f'user_{json_user.id}_recent_tracks')
     for track in json_recent_tracks:
-        json_track = source.get('track', False, track.artist, track.title)
+        json_track = source.get('track', track.artist, track.title)
         storage.write(json_track, f'track_{json_track.id}')
