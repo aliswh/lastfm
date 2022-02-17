ed5d04df968dbe9cf405bf661ebe2fcfc7a58fb8
Davide D'Ascenzo <davide.dascenzo.work@gmail.com>
2022-02-17 16:07:30 +0100

    Update batchwriter.py

--- src/ingestion_layer/batchwriter.py
+++ src/ingestion_layer/batchwriter.py
@@ -9,7 +9,7 @@ class BatchWriter(Writer):
         users = self.source.get('user_pool',seed_user,MAX=users_limit)
         for user in users:
             if debug:
-                print(f'User:{user}')
+                print(f'[User] {user}')
             self.dest.write(user, dest_path+f"users/user_{user['id']}")
             recent_tracks = self.source.get('recent_tracks',user['user'],limit=tracks_limit)
             self.dest.write(recent_tracks, dest_path+f"recent_tracks/user_recent_tracks_{user['id']}")
@@ -19,5 +19,6 @@ class BatchWriter(Writer):
                 self.dest.write(track, dest_path+f"tracks/track_{track['id']}")
                 self.dest.write(artist, dest_path+f"artists/artist_{artist['id']}")
                 if debug:
-                    print(f'Track:{track}')
-                    print(f'Artist:{artist}')
+                    print(f'[Track] {track}')
+                    print(f'[Artist] {artist}')
+                    print()
