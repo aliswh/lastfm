1c4f100f194ba0880dfac9e8ace9a4f11a0124fc
Davide D'Ascenzo <davide.dascenzo.work@gmail.com>
2022-02-17 15:57:40 +0100

    Updated __init__ and config

--- src/ingestion_layer/config.py
+++ src/ingestion_layer/config.py
@@ -1,9 +1,15 @@
+#Google Storage config
 bucket_name = 'erudite-host-341412.appspot.com'
 creds_path = 'erudite-host-341412-a0961664e5cd.json'
 
+#Pylast config
 AUTH_DATA = {
   'API_KEY' : 'b9d6b46e0fd8d63dc432ea1642924a5b',
   'API_SECRET' : "e560c18c8f230f8a9ffb3423c1ccae0b",
   'username' : "aliswh",
   'password' : "Music4!" #TODO: security?
 }
+
+#BatchWriter config
+users_limit = 10
+tracks_limit = 10
