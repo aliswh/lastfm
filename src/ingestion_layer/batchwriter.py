42b0caf1d509f997b73d1030108adf2d48fc0f65
Davide D'Ascenzo <davide.dascenzo.work@gmail.com>
2022-02-17 15:55:49 +0100

    Decoupled batchwriter and updated googlestorage

---
+++ src/ingestion_layer/batchwriter.py
@@ -0,0 +1,23 @@
+from .abstract_ingestion_layer import Writer
+
+class BatchWriter(Writer):
+    def __init__(self, source, destination):
+        self.source = source
+        self.dest = destination
+
+    def write(self, dest_path, seed_user, users_limit, tracks_limit, debug = False):
+        users = self.source.get('user_pool',seed_user,MAX=users_limit)
+        for user in users:
+            if debug:
+                print(f'User:{user}')
+            self.dest.write(user, f"users/user_{user['id']}")
+            recent_tracks = self.dest.get('recent_tracks',user['user'],limit=tracks_limit)
+            self.dest.write(recent_tracks, f"recent_tracks/user_recent_tracks_{user['id']}")
+            for track in recent_tracks[1:]:
+                track = self.source.get('track', track['artist'], track['title'])
+                artist = self.source.get('artist', track['artist'])
+                self.dest.write(track, f"tracks/track_{track['id']}")
+                self.dest.write(artist, f"artists/artist_{artist['id']}")
+                if debug:
+                    print(f'Track:{track}')
+                    print(f'Artist:{artist}')
